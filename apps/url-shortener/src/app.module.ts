e41c9a477ac463f64da168ff00a9fa6ce0d24e44
CSenshi <saba.pochkhua@gmail.com>
2025-06-15 22:41:29 +0400

    feat: refactor app module and create url module for better organization

--- apps/url-shortener/src/app.module.ts
+++ apps/url-shortener/src/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ShortenUrl } from './url/commands/shorten-url';
 import { LoggerModule } from 'nestjs-pino';
 import { CqrsModule } from '@nestjs/cqrs';
-import { GetRealUrl } from './url/queries/get-real-url';
-import { IdObfuscatorService } from './url/app-services/id-obfuscator.service';
-import { NumberHasherService } from './url/app-services/number-hasher.service';
-import { CounterModule } from './counter/counter.module';
-import { UrlRepository } from './url/repositories/url.repository';
 
 @Module({
   imports: [
-    CounterModule,
     CqrsModule.forRoot(),
     LoggerModule.forRoot({
       pinoHttp: {
@@ -23,14 +16,6 @@ import { UrlRepository } from './url/repositories/url.repository';
             : undefined,
       },
     }),
-  ],
-  controllers: [ShortenUrl.HttpController, GetRealUrl.HttpController],
-  providers: [
-    ShortenUrl.Service,
-    GetRealUrl.Service,
-    IdObfuscatorService,
-    UrlRepository,
-    NumberHasherService,
   ],
 })
 export class AppModule {}
