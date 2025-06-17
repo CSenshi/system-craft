c5cceaba5feacf1519a69a7a5482f87a293604c8
CSenshi <saba.pochkhua@gmail.com>
2025-06-17 12:51:59 +0400

    fix: add missing ConfigModule to url module

--- apps/url-shortener/src/url/url.module.ts
+++ apps/url-shortener/src/url/url.module.ts
@@ -5,9 +5,10 @@ import { GetRealUrl } from './queries/get-real-url';
 import { IdObfuscatorService } from './app-services/id-obfuscator.service';
 import { NumberHasherService } from './app-services/number-hasher.service';
 import { UrlRepository } from './repositories/url.repository';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CounterModule],
+  imports: [CounterModule, ConfigModule.forRoot()],
   controllers: [ShortenUrl.HttpController, GetRealUrl.HttpController],
   providers: [
     ShortenUrl.Service,
