49cd5625c87a592d9510d88fa4459853dc3629ce
CSenshi <saba.pochkhua@gmail.com>
2025-10-15 00:08:23 +0400

    chore(url-shortener): add cwd to infra commands

--- apps/url-shortener/src/app.module.ts
+++ apps/url-shortener/src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ThrottlerModule, seconds } from '@nestjs/throttler';
 import {
-  Redis,
   RedisModule,
   RedisThrottlerStorage,
   RedisToken,
 } from '@nestjs-redis/kit';
 import { LoggerModule } from 'nestjs-pino';
+import { RedisClientType } from 'redis';
 import { PrismaModule } from './prisma/prisma.module';
 import { UnhandledExceptionsListener } from './unhandled-exceptions.listener';
 import { UrlModule } from './url/url.module';
 
 @Module({
   imports: [
+    RedisModule.forRoot({
+      options: { url: process.env['REDIS_HOST'] },
+      isGlobal: true,
+    }),
     PrismaModule,
     CqrsModule.forRoot(),
     LoggerModule.forRoot({
@@ -28,17 +32,15 @@ import { UrlModule } from './url/url.module';
       },
     }),
     ThrottlerModule.forRootAsync({
-      imports: [
-        RedisModule.forRoot({ options: { url: process.env['REDIS_HOST'] } }),
-      ],
       inject: [RedisToken()],
-      useFactory: (redis: Redis) => ({
+      useFactory: (redis: any) => ({
         throttlers: [{ limit: 1, ttl: seconds(10) }],
         storage: new RedisThrottlerStorage(redis),
       }),
     }),
     UrlModule,
   ],
+  controllers: [],
   providers: [
     UnhandledExceptionsListener,
     // {
